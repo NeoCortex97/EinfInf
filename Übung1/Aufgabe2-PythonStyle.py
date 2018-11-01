0337d74afe5e58892f1aa137d9c0d272de86d401
StefanSchmelz <stefan_schmelz@gmx.net>
2018-11-01 14:59:20 +0100

    Added

---
+++ Übung1/Aufgabe2-PythonStyle.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# this is the ugly python style solution to task 2.
+
+a = int(input("A: "))
+b = int(input("B: "))
+print(" " + ("-" * a) + "\n" + (("|" + (" " * a) + "|\n") * b) + " " + ("-" * a))
